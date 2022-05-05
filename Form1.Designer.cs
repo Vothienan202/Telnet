ea99cad5e46812c793a30dc268f5e5b65fdacc8b
Vothienan202 <90508803+Vothienan202@users.noreply.github.com>
2022-05-05 09:30:33 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Telnet
+{
+    partial class TelnetServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListen = new System.Windows.Forms.Button();
+            this.listViewCommand = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(466, 53);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(172, 35);
+            this.btnListen.TabIndex = 0;
+            this.btnListen.Text = "Listen";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listViewCommand
+            // 
+            this.listViewCommand.HideSelection = false;
+            this.listViewCommand.Location = new System.Drawing.Point(122, 119);
+            this.listViewCommand.Name = "listViewCommand";
+            this.listViewCommand.Size = new System.Drawing.Size(516, 265);
+            this.listViewCommand.TabIndex = 1;
+            this.listViewCommand.UseCompatibleStateImageBehavior = false;
+            // 
+            // TelnetServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listViewCommand);
+            this.Controls.Add(this.btnListen);
+            this.Name = "TelnetServer";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.ListView listViewCommand;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Telnet
+{
+    public partial class TelnetServer : Form
+    {
+        public TelnetServer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread serverThread = new Thread(new ThreadStart(StartUnsafeThread));
+            serverThread.Start();
+        }
+
+        void StartUnsafeThread()
+        {
+            int bytesReceived = 0;
+            byte[] recv = new byte[1];
+            Socket clientSocket;
+
+            Socket listenerSocket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+                );
+        IPEndPoint ipepServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            listenerSocket.Bind(ipepServer);
+            listenerSocket.Listen(-1);
+            clientSocket = listenerSocket.Accept();
+            listViewCommand.Items.Add(new ListViewItem("Telnet running on 17.0.0.1:8080"));
+            listViewCommand.View = View.List;
+            while (clientSocket.Connected)
+            {
+                string text = "";
+                do
+                {
+                    bytesReceived = clientSocket.Receive(recv);
+                    text += Encoding.ASCII.GetString(recv);
+                } while (text[text.Length - 1] != '\n');
+                listViewCommand.Items.Add(new ListViewItem(text));
+            }
+            listenerSocket.Close();
+                
+        }
+    }
+}
